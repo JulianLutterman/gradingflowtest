4888b6b5d496c3e5840a0d3afbbc73a82b82940b
Julian Lutterman <juulruijter@gmail.com>
2025-09-03 21:01:36 +0200

    Update editing.js

--- editing.js
+++ editing.js
@@ -184,7 +184,7 @@ function toggleEditMode(container, isEditing, fields = null, editButtonParam = n
     }
   } else {
       if (editButton) editButton.classList.remove('hidden');
-      if (editActions) editActions.remove();
+      if (editActions) editActions.classList.add('hidden');
 
       const targetType = editButton?.dataset.editTarget;
       if (targetType === 'sub_question') {
